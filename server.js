9a7711fa46a6d058876745e8a94b534639922df3
Conquerors & Coheirs of Christ <conquerorscoheirsag@gmail.com>
2025-12-21 14:21:56 +0800

    Configure N8N to use HTTPS protocol
    Set N8N protocol to HTTPS for secure communication.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// Render sets a dynamic port, n8n needs it
 process.env.N8N_PORT = process.env.PORT;
 process.env.N8N_HOST = process.env.RENDER_EXTERNAL_HOSTNAME;
+process.env.N8N_PROTOCOL = 'https';
 process.env.WEBHOOK_URL = `https://${process.env.RENDER_EXTERNAL_HOSTNAME}/`;
 
 // Start n8n
