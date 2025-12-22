ba4721df8fbbe9279a1e6daf09da2ac630ed6884
Conquerors & Coheirs of Christ <conquerorscoheirsag@gmail.com>
2025-12-22 18:00:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-process.env.N8N_PORT = process.env.PORT;
+// server.js
+process.env.N8N_PORT = process.env.PORT || 5678;
 process.env.N8N_HOST = process.env.RENDER_EXTERNAL_HOSTNAME;
 process.env.N8N_PROTOCOL = 'https';
 process.env.WEBHOOK_URL = `https://${process.env.RENDER_EXTERNAL_HOSTNAME}/`;
 
-// Start n8n
+// Force n8n to use Postgres if the variables are set
+if (process.env.DB_POSTGRESDB_HOST) {
+  process.env.DB_TYPE = 'postgresdb';
+}
+
 require('n8n/bin/n8n');
